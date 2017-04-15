7a736f315cb6c8f912750f0546d0d546bd348993
Magnus Wallin <magnus.wlln@gmail.com>
2017-04-15 06:32:20 +0200

    Ja

---
+++ src/Robot.java
@@ -0,0 +1,28 @@
+/**
+ * Created by Magnus on 2017-04-14.
+ */
+public class Robot {
+    private Position position;
+    protected Maze maze;
+
+    public Robot(Maze maze){
+
+    }
+
+    public void Move(){
+
+    }
+
+    public Position getCurrentPosition() {
+
+    }
+
+    protected void setCurrentPosition(Position){
+
+    }
+
+    public boolean hasReachedGoal(){
+
+    }
+
+}
